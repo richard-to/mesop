43b46a39964b41793e762a5157caa07b480c207a
Richard To <gohn76@gmail.com>
2025-12-24 14:16:37 -0800

    Fix navigate.py page paths to be more unique (#1329)

--- mesop/examples/navigate.py
+++ mesop/examples/navigate.py
@@ -2,15 +2,15 @@ import mesop as me
 
 
 def navigate(event: me.ClickEvent):
-  me.navigate("/about")
+  me.navigate("/examples/navigate/about")
 
 
-@me.page(path="/")
+@me.page(path="/examples/navigate/home")
 def home():
   me.text("This is the home page")
   me.button("navigate to about page", on_click=navigate)
 
 
-@me.page(path="/about")
+@me.page(path="/examples/navigate/about")
 def about():
   me.text("This is the about page")
