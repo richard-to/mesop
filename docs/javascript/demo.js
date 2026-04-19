1809225a87e1c336092e6abd204de52c91f3ea77
Richard To <539889+richard-to@users.noreply.github.com>
2026-04-18 17:21:12 -0700

    Update Mesop Demo page url (#1380)

--- docs/javascript/demo.js
+++ docs/javascript/demo.js
@@ -2,6 +2,6 @@ const urlParams = new URLSearchParams(window.location.search);
 const demoParam = urlParams.get('demo') || '';
 
 const iframe = document.createElement('iframe');
-iframe.src = 'https://wwwillchen-mesop.hf.space/' + demoParam;
+iframe.src = 'https://richard-to-mesop.hf.space/' + demoParam;
 iframe.className = 'full-screen-iframe';
 document.body.appendChild(iframe);
